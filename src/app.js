e5115868601b26b1060403f901720a350378766e
Zubiik <elodiefrappat08@gmail.com>
2023-08-28 14:28:03 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
 const http = require('http');
 const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
+const express = require('express');
+const app = express();
+app.use('/add', (req, res, next) => {
+   res.send('<div>hello</div>');
 });
-
-server.listen(3010);
-
+app.use('/',(req, res, next) => {
+  res.send('<div>hi</div>');
+});
+//same 
+//const server = http.createServer(app);
+//server.listen(3010);
+app.listen(3010);
 
 
